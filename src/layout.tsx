51242d372736920a6e1e4a3547d1ba2b905b03f9
Neko Developer <nekkochan2k@gmail.com>
2024-11-28 05:23:38 +0700

    Fix F5 and Loading

--- src/layout.tsx
+++ src/layout.tsx
@@ -1,12 +1,45 @@
 import {Outlet} from "react-router-dom";
 import AppHeader from "components/layout/app.header";
+import {useEffect} from "react";
+import {fetchAccountAPI} from "services/api.ts";
+import {useCurrentApp} from "components/context/app.context.tsx";
+import PacmanLoader from "react-spinners/PacmanLoader";
 
 const Layout = () => {
+    const {setUser, isLoading, setIsLoading, setIsAuthenticated} = useCurrentApp();
+    useEffect(() => {
+        const fetchAccount = async () => {
+            const res = await fetchAccountAPI();
+            if (res.data) {
+                setUser(res.data.user);
+                setIsAuthenticated(true);
+            }
+            setIsLoading(false);
+        }
+        fetchAccount();
+    }, []);
 
     return (
         <>
-            <AppHeader/>
-            <Outlet/>
+            {!isLoading ?
+                <div>
+                    <AppHeader/>
+                    <Outlet/>
+                </div>
+                :
+                <div style={{
+                    position: "fixed",
+                    top: "50%",
+                    left: "50%",
+                    transform: "translate(-50%, -50%)"
+                }}
+                >
+                    <PacmanLoader
+                        size={30}
+                        color={"#36d6b4"}
+                    />
+                </div>
+            }
         </>
     )
 }
