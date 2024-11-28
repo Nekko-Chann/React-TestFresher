78aa27f6703c8e2572802dd68f924ea0036b0ed5
Neko Developer <nekkochan2k@gmail.com>
2024-11-28 08:20:53 +0700

    Design Layout Admin

--- src/layout.tsx
+++ src/layout.tsx
@@ -1,46 +1,11 @@
 import {Outlet} from "react-router-dom";
 import AppHeader from "components/layout/app.header";
-import {useEffect} from "react";
-import {fetchAccountAPI} from "services/api.ts";
-import {useCurrentApp} from "components/context/app.context.tsx";
-import PacmanLoader from "react-spinners/PacmanLoader";
 
 const Layout = () => {
-    const {setUser, isLoading, setIsLoading, setIsAuthenticated} = useCurrentApp();
-
-    useEffect(() => {
-        const fetchAccount = async () => {
-            const res = await fetchAccountAPI();
-            if (res.data) {
-                setUser(res.data.user);
-                setIsAuthenticated(true);
-            }
-            setIsLoading(false);
-        }
-        fetchAccount();
-    }, []);
-
     return (
         <>
-            {isLoading === false ?
-                <div>
-                    <AppHeader/>
-                    <Outlet/>
-                </div>
-                :
-                <div style={{
-                    position: "fixed",
-                    top: "50%",
-                    left: "50%",
-                    transform: "translate(-50%, -50%)"
-                }}
-                >
-                    <PacmanLoader
-                        size={30}
-                        color={"#36d6b4"}
-                    />
-                </div>
-            }
+            <AppHeader/>
+            <Outlet/>
         </>
     )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -9,8 +9,13 @@ import LoginPage from 'pages/client/auth/login';
 import RegisterPage from 'pages/client/auth/register';
 import 'styles/global.scss'
 import HomePage from 'pages/client/home';
-import {AppProvider} from "components/context/app.context.tsx";
+import {AppProvider} from "components/context/app.context";
 import ProtectedRoute from "components/auth";
+import LayoutAdmin from "components/layout/layout.admin";
+import DashBoardPage from "pages/admin/dashboard";
+import ManageBookPage from "pages/admin/manage.book";
+import ManageOrderPage from "pages/admin/manage.order";
+import ManageUserPage from "pages/admin/manage.user";
 
 const router = createBrowserRouter([
     {
@@ -37,11 +42,49 @@ const router = createBrowserRouter([
                     </ProtectedRoute>
                 ),
             },
+        ]
+    },
+    {
+        path: "admin",
+        element: <LayoutAdmin />,
+        children: [
+            {
+                index: true,
+                element: (
+                    <ProtectedRoute>
+                        <DashBoardPage />
+                    </ProtectedRoute>
+                )
+            },
+            {
+                path: "book",
+                element: (
+                    <ProtectedRoute>
+                        <ManageBookPage />
+                    </ProtectedRoute>
+                )
+            },
+            {
+                path: "order",
+                element: (
+                    <ProtectedRoute>
+                        <ManageOrderPage />
+                    </ProtectedRoute>
+                )
+            },
+            {
+                path: "user",
+                element: (
+                    <ProtectedRoute>
+                        <ManageUserPage />
+                    </ProtectedRoute>
+                ),
+            },
             {
                 path: "/admin",
                 element: (
                     <ProtectedRoute>
-                        <div>Admin Page</div>
+                        <div>admin page</div>
                     </ProtectedRoute>
                 ),
             }
